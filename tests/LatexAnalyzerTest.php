13dda215b38ae85e9d5e5e00a41b1e9c11a068a9
Guto Maia <guto@guto.net>
2011-12-16 14:13:47 -0200

    removed the loader from the LatexAnalyzerTest case and put it on the loader.php. Also added a tdd.sh script for testing

--- tests/LatexAnalyzerTest.php
+++ tests/LatexAnalyzerTest.php
@@ -1,16 +1,5 @@
 <?php
 
-function autoload($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $file = __DIR__ . "/../lib/$class.php";
-    if(file_exists($file)) {
-        require_once($file);
-        return true;
-    }    
-}
-
-spl_autoload_register("autoload");
-
 class LatexAnalyzerTest extends PHPUnit_Framework_TestCase {
     public $latex;
     
---
+++ tests/loader.php
@@ -0,0 +1,12 @@
+<?php
+
+function autoload($class) {
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    $file = __DIR__ . "/../lib/$class.php";
+    if(file_exists($file)) {
+        require_once($file);
+        return true;
+    }    
+}
+
+spl_autoload_register("autoload");
